0452491692cbf47f464aff0b9bd2bbf3fcde5b75
Abdel Hamed <abdelhamedreda@outlook.com>
2026-04-18 00:42:09 +0200

    add offline feature

--- src/hooks/useOnlineStatus.js
+++ src/hooks/useOnlineStatus.js
@@ -5,21 +5,25 @@ export default function useOnlineStatus() {
   const [showBackOnline, setShowBackOnline] = useState(false);
 
   useEffect(() => {
+    const handleOffline = () => {
+      setIsOnline(false);
+    };
+
     const handleOnline = () => {
       setIsOnline(true);
       setShowBackOnline(true);
+
       setTimeout(() => {
         setShowBackOnline(false);
       }, 2000);
     };
-    const handleOffline = () => {
-      setIsOnline(false);
-    };
-    window.addEventListener("online", handleOnline);
+
     window.addEventListener("offline", handleOffline);
+    window.addEventListener("online", handleOnline);
+
     return () => {
-      window.removeEventListener("online", handleOnline);
       window.removeEventListener("offline", handleOffline);
+      window.removeEventListener("online", handleOnline);
     };
   }, []);
 
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Nav from '../../components/Nav/Nav'
+import useOnlineStatus from '../../hooks/useOnlineStatus';
 
 export default function Layout() {
+  const { isOnline, showBackOnline } = useOnlineStatus();
 
   return (
     <div className='bg-bg dark:bg-bg-dark min-h-screen'>
 
-     
+    <div
+  className={`overflow-hidden transition-all duration-500
+  ${!isOnline || showBackOnline ? "max-h-20" : "max-h-0"}
+  `}
+>
+  <div
+    className={`w-full text-center py-2 font-semibold
+    ${!isOnline ? "bg-red-600 text-white" : "bg-green-600 text-white"}
+    `}
+  >
+    {!isOnline ? "❌ You are offline" : "✅ Back online"}
+  </div>
+</div>
 
 
       <Nav />
