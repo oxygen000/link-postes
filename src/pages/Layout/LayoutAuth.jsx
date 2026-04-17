23c255fda9b1b084f209a70bb069c407b2efea73
Abdel Hamed <abdelhamedreda@outlook.com>
2026-04-18 00:28:53 +0200

    remove react-offline + add offline feature

--- src/pages/Layout/LayoutAuth.jsx
+++ src/pages/Layout/LayoutAuth.jsx
@@ -1,29 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import useOnlineStatus from '../../hooks/useOnlineStatus'
 
 
 export default function LayoutAuth() {
-    const { isOnline, showBackOnline } = useOnlineStatus();
   
   return (
     <>
-
-      const { isOnline, showBackOnline } = useOnlineStatus();
-    
-       {/* 🔴 Offline */}
-      {!isOnline && (
-        <div className="bg-red-600 text-white text-center py-2">
-          ❌ No Internet Connection
-        </div>
-      )}
-
-      {/* 🟢  Online */}
-      {isOnline && showBackOnline && (
-        <div className="bg-green-600 text-white text-center py-2">
-          ✅ Connection Restored
-        </div>
-      )}
         <Outlet/>
 
     </>
